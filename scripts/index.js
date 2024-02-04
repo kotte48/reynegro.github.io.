ddaa9ef519117fb28b0494f933018916b4f03e47
Alexandr <72464406+kotte48@users.noreply.github.com>
2024-02-04 15:36:44 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".menu-burger"),t=document.querySelector(".main-nav");e.addEventListener("click",()=>{e.classList.toggle("menu-burger--active"),t.classList.toggle("main-nav--active")});
